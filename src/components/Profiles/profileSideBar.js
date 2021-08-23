584e29e37bcf72190ab1ea5d44160c107bfc2d14
ilyas <larhdidok@gmail.com>
2021-08-23 23:35:56 +0100

    fixed bug

--- src/components/Profiles/profileSideBar.js
+++ src/components/Profiles/profileSideBar.js
@@ -3,22 +3,36 @@ import { Badge } from 'antd';
 import { Link } from "react-router-dom";
 import { useQuery } from "react-query";
 import properties from "../../properties";
+import { useCookies } from "react-cookie";
 
 const {host} = properties;
-const url = `${host}/api/v1/users/page/0`;
+//const url = `${host}/api/v1/users/page/0`;
 const fetchData = async (key)=>{
     const page = key.queryKey[1];
-    const res = await fetch(`${host}/api/v1/users/page/${page}`)
+    const token = key.queryKey[2];
+    const res = await fetch(`${host}/api/v1/users/page/${page}`,{
+        headers: {
+            'Content-Type' : 'application/json',
+            'Authorization': token
+        }
+    } )
     return res.json();
 }
 const ProfileSideBar = ()=>{
+    const [cookies] = useCookies([]);
+    let token = "";
+        
+    if(cookies.ilyToken != null)
+        token = cookies.ilyToken;
 
-    const {data,isLoading} = useQuery(['page',0],fetchData,{keepPreviousData:true})
-
+    const {data,isLoading} = useQuery(['page',0,token],fetchData,{keepPreviousData:true})
+    console.log("my dataaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+    console.log(data);
     return(
     <>
     {/* show friends but for now i will show all users */}
-    {
+    {isLoading && <div>loadning....</div>}
+    {data && 
         data.content.map((user)=>{
             return (<div><b><Link to={`/profile/${user.id}`}>{user.firstName} {user.lastName}</Link></b> <Badge status="success"><Avatar>IS</Avatar></Badge></div>)
         })
